964ae4a520f823cb1f665fbe4b387736504e6535
Gingertonic <bethmschofield@gmail.com>
2020-04-25 18:00:03 +0100

    stub fetchFeatured req

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Play() {
+    const [loading, setLoading] = useState(false) 
+    const [featured, setFeatured] = useState([])
+
+    useEffect(() => {
+        const fetchFeatured = async () => {
+            setLoading(true)
+            const resp = await fetch('/api/featured')
+            const featured = await resp.json()
+            setFeatured(featured)
+            setLoading(false)
+        }
+        fetchFeatured()
+    }, [])
+
     return (
         <div>
            <h1>Play</h1> 
