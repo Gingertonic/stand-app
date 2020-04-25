981d44ae6ccbf257d30b812a2779807a2742165f
Gingertonic <bethmschofield@gmail.com>
2020-04-25 18:27:21 +0100

    stub patch request on user reveal

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Feature({ sub }) {
+    const { imageUrl, name } = sub.data
+    const imageId = sub.ref['@ref'].id
+    const [revealed, setRevealed] = useState(false)
+
+    const handleRevealClick = async () => {
+        const resp = await fetch('/api/update', {
+            method: 'PATCH',
+            body: JSON.stringify({ id: imageId })
+        })
+        const { error, message } = resp.json()
+        error ? console.error('Something went wrong...') : setRevealed(true)
+    }
+
     return (
         <div className="featured">
-            Feature Image
-            <img src="" alt="feature"/>
+            <img src={imageUrl} alt="feature"/>
+            <code>:belongs_to </code><button onClick={handleRevealClick} disabled={revealed}>{revealed ? name : 'Reveal'}</button>
         </div>
     )
 }
