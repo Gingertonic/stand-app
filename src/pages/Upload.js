f92358fbda93d0998e251ac53ec39c8c0e373b79
Gingertonic <bethmschofield@gmail.com>
2020-04-25 16:23:08 +0100

    add upload to cloudinary

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -8,8 +8,19 @@ export default function Upload() {
 
     const handleNameChange = e => setName(e.target.value)
 
-    const handleUploadClick = () => {
+    const handleUploadClick = async () => {
+        const imageData = new FormData()
+        imageData.append('upload_preset', 'stand-app')
+        imageData.append('file', file)
         // 1. store the image on eg. Cloudinary, get the link 
+        const resp = await fetch('https://api.cloudinary.com/v1_1/gingertonic-fis/upload', {
+            method: 'POST',
+            body: imageData
+        })
+
+        const data = await resp.json()
+        const imageUrl = data.url
+
         // 2. send the image link and user's name to our storage
     }
 
